23fb5d48732cba4fa5830ed01749cbf592545e31
Gabor Spaits <gaborspaits1@gmail.com>
2023-01-30 19:58:15 +0100

    Assess the capabilities of analyzer

---
+++ clang/test/Analysis/std-any-test.cpp
@@ -0,0 +1,73 @@
+// RUN: %clang %s -Xclang -verify --analyze \
+// RUN:   -Xclang -analyzer-checker=core \
+// RUN:   -Xclang -analyzer-checker=debug.ExprInspection \
+// RUN:   -Xclang -analyzer-checker=alpha.core.StdVariant
+
+#include <any>
+#include <string>
+#define MAGIC_NUMBER 21
+
+class DummyClass {
+  public:
+  void f() {};
+};
+
+struct LargeClass {
+    public:
+    long long elem, a, b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,asasasasa, aa, ab, aaa, ls, qwert, trewq, asd, ds;
+    char bigStringOne[10000] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    char bigStringTwo[10000] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+};
+
+void clang_analyzer_eval(int);
+void clang_analyzer_warnIfReached();
+template <typename T>
+constexpr void clang_analyzer_dump(T) {}
+
+template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
+void reasonAboutValueHeldInAny() {
+  std::any a = MAGIC_NUMBER;
+  int valueFromAny = std::any_cast<int> (a);
+  clang_analyzer_eval(valueFromAny == MAGIC_NUMBER); // expected-warning{{TRUE}}
+}
+void reasonAboutBigValueHeldInAny() {
+  LargeClass lc{MAGIC_NUMBER};
+  std::any a = lc;
+  LargeClass valueFromAny = std::any_cast<LargeClass> (a);
+  clang_analyzer_eval(valueFromAny.elem == MAGIC_NUMBER); // expected-warning{{TRUE}}
+}
+
+// check weather the analyzer can reason about primitive types in std::any
+void reasoningAboutTypeHeldInAny() {
+  std::any a = MAGIC_NUMBER;
+
+  int i = std::any_cast<int> (a);
+  clang_analyzer_warnIfReached(); // expected-warning{{REACHABLE}}
+  // if we reach this point that means the analyzer knows that the std::any holds an int
+
+  DummyClass d = std::any_cast<DummyClass> (a);
+  clang_analyzer_warnIfReached(); // no-warning
+  // if we won't reach this point that means the analyzer
+  // has stopped the construction of Exploided Graph in this path
+  // becasue it knows that any_cast function call will fail
+  d.f();
+  i++;
+}
+
+// check weather the analyzer can reason about large types in std::any
+void reasonAboutBigTypeHeldInAny() {
+  LargeClass lc{MAGIC_NUMBER};
+  std::any a = lc;
+
+  LargeClass lv2 = std::any_cast<LargeClass> (a);
+  clang_analyzer_warnIfReached(); // expected-warning{{REACHABLE}}
+  // if we reach this point that means the analyzer knows that the std::any
+  // holds a LargeClass user defined type
+
+  int d = std::any_cast<int> (a);
+  clang_analyzer_warnIfReached(); // no-warning
+  d++;
+  lv2.elem++;
+}
--- clang/test/Analysis/std-variant-checker.cpp
+++ clang/test/Analysis/std-variant-checker.cpp
@@ -1,7 +1,66 @@
-// RUN: %clang_analyze_cc1 -analyzer-checker=core,alpha.core.StdVariant %s -verify
+// RUN: %clang %s -Xclang -verify --analyze \
+// RUN:   -Xclang -analyzer-checker=core \
+// RUN:   -Xclang -analyzer-checker=debug.ExprInspection \
+// RUN:   -Xclang -analyzer-checker=alpha.core.StdVariant
 
-#include "Inputs/variant.h"
+#include <variant>
+#include <string>
+#define MAGIC_NUMBER 21
+
+class DummyClass {
+  public:
+  void f() {};
+};
+
+void clang_analyzer_eval(int);
+void clang_analyzer_warnIfReached();
+template <typename T>
+constexpr void clang_analyzer_dump(T) {}
+
+template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
 void g() {
   std::variant<int, char> v; // expected-warning{{Variant Created [alpha.core.StdVariant]}}
 }
+
+void reasonAboutValueHeldInVariant() {
+  std::variant<int, char> v = MAGIC_NUMBER;
+
+  int numberFromVariant = std::get<int>(v);
+  clang_analyzer_warnIfReached(); // expected-warning{{REACHABLE}}
+  clang_analyzer_eval(numberFromVariant == MAGIC_NUMBER); // expected-warning{{TRUE}}
+
+  DummyClass a = std::get<DummyClass>(v);
+  clang_analyzer_warnIfReached(); // no-warning
+
+}
+
+void reasoningAboutGetIf() {
+  std::variant<int, DummyClass> v = MAGIC_NUMBER;
+  DummyClass* inVariant = std::get_if<DummyClass>(&v);
+  clang_analyzer_eval(inVariant == nullptr); // expected-warning{{TRUE}}
+  inVariant->f();
+  clang_analyzer_warnIfReached(); // no-warning
+}
+
+void visit() {
+  std::variant<int, DummyClass> v = MAGIC_NUMBER;
+
+  int res = std::visit(overloaded{[](auto arg) {
+                                        clang_analyzer_warnIfReached(); // no-warning
+                                        return 0;
+                                        },
+                                  [](double arg) {
+                                        clang_analyzer_warnIfReached(); // no-warning
+                                        return 1; 
+                                        },
+                                  [](int arg) {
+                                        clang_analyzer_warnIfReached(); // expected-warning {{REACHABLE}}
+                                        return 2; 
+                                        }},
+                      v);
+  clang_analyzer_dump(res);
+  clang_analyzer_eval(res == 2); // expected-warning{{TRUE}}
+}
+
