b3178c1b114d694f5b4e2973e6f3a5e26a63754e
Gabor Spaits <gaborspaits1@gmail.com>
2023-03-04 09:51:21 +0100

    actually adding the tc

---
+++ clang/test/Analysis/std-variant.cpp
@@ -0,0 +1,30 @@
+// RUN: %clang %s -Xclang -verify --analyze \
+// RUN:   -Xclang -analyzer-checker=core \
+// RUN:   -Xclang -analyzer-checker=debug.ExprInspection
+
+#include <variant>
+#include <string>
+#define MAGIC_NUMBER 21
+
+class DummyClass {
+  public:
+  void f() {};
+};
+
+void clang_analyzer_eval(int);
+void clang_analyzer_warnIfReached();
+template <typename T>
+constexpr void clang_analyzer_dump(T) {}
+
+void g() {
+  clang_analyzer_warnIfReached();  // expected-warning{{REACHABLE}}
+  std::variant<int, char> v; 
+  clang_analyzer_warnIfReached();  // expected-warning{{REACHABLE}}
+  v = 0;
+  clang_analyzer_warnIfReached();  // expected-warning{{REACHABLE}}
+  int numFormVarinat = std::get<int>(v);
+  clang_analyzer_warnIfReached();  // expected-warning{{REACHABLE}}
+  int result = 25/ numFormVarinat; // expected-warning {{Division by zero [core.DivideZero]}}
+  result++;
+}
+
