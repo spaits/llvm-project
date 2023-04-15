9c30b5d956c264708a9adc373f82b77d3003e303
Gabor Spaits <gaborspaits1@gmail.com>
2023-04-15 17:39:53 +0200

    refactor

--- clang/lib/StaticAnalyzer/Checkers/VariantLikeTypeModeling.h
+++ clang/lib/StaticAnalyzer/Checkers/VariantLikeTypeModeling.h
@@ -14,6 +14,9 @@ namespace clang {
 namespace ento {
 namespace variant_modeling {
 
+
+// The implementation of all these functions can be found in the
+// StdVariantChecker.cpp file under the same directory as this file.
 const TemplateArgument& getFirstTemplateArgument(const CallEvent &Call);
 bool isObjectOf(QualType t, QualType to);
 bool isCopyConstructorCallEvent (const CallEvent& Call);
@@ -26,7 +29,13 @@ bool isStdVariant(const Type *Type);
 bool isStdAny(const Type *Type);
 
 template <class T>
-void bindFromVariant(const BinaryOperator *BinOp, CheckerContext &C, const CallDescription &StdGet) {
+
+// We handle the retrieving of objects from an std::variant or std::any
+void bindFromVariant(const BinaryOperator *BinOp,
+                     CheckerContext &C,
+                     const CallDescription &StdGet) {
+  // First we check if the right hand site of the call is matches the
+  // CallDescription we gave as argument.
   if (!BinOp->isAssignmentOp()) {
     return;
   }
@@ -60,31 +69,35 @@ void bindFromVariant(const BinaryOperator *BinOp, CheckerContext &C, const CallD
     return;
   }
   // We know that at this point we assign value to the LValue on the left from
-  // and std::variant
+  // and a call we want.
 
-  // what if we assign value to std::variant or std::any the map should still
-  // be updated  
   auto Arg = RHSCall->getArg(0);
   if (!Arg) {
     return;
   }
-  Arg->dump();
   auto ArgDeclRef = dyn_cast<DeclRefExpr>(Arg);
   auto VDecl = dyn_cast<VarDecl>(ArgDeclRef->getDecl());
 
 
   auto ArgSVal = C.getStoreManager().getLValueVar(VDecl, C.getLocationContext());//C.getSVal(Arg);
+  // In ArgMemRegion we have the memory region of the calls argument.
+  // The call in our case is an std::get with an std::variant argument
+  // or an std::any_case with an std::any argument.
   auto ArgMemRegion = ArgSVal.getAsRegion();
   if (!ArgMemRegion) {
     return;
   }
+
   auto State = C.getState();
   //add check if
+  //  We get the value held in std::variant or std::any.
   auto SValGet = State->get<T>(ArgMemRegion);
   if (!SValGet) {
     return;
   }
 
+  // Now we get the memory region for the LValue we assign the result of
+  // std::get or std::any_cast call to.
   auto LeftHandExpr = BinOp->getLHS();
   auto LHSSVal = C.getSVal(LeftHandExpr);
   auto LHSLoc = dyn_cast<Loc>(LHSSVal);
@@ -103,8 +116,9 @@ void handleConstructorAndAssignment(const CallEvent &Call,
                                       CheckerContext &C,
                                       const SVal &thisSVal) {
   ProgramStateRef State = Call.getState(); // check
-  auto argQType = Call.getArgSVal(0).getType(C.getASTContext());
-  const Type* ArgTypePtr = argQType.getTypePtr();
+
+  auto ArgQType = Call.getArgSVal(0).getType(C.getASTContext());
+  const Type* ArgTypePtr = ArgQType.getTypePtr();
   auto ThisRegion = thisSVal.getAsRegion();
   auto ArgSVal = Call.getArgSVal(0);
   auto AsMemRegSVal = dyn_cast<Loc>(ArgSVal);
@@ -116,23 +130,60 @@ void handleConstructorAndAssignment(const CallEvent &Call,
   auto ArgMemRegion = Call.getArgSVal(0).getAsRegion();
 
   State = [&]() {
-    if (isCopyConstructorCallEvent(Call) ||
-                                          isCopyAssignmentOperatorCall(Call)) {
-      // if the argument of a copy constructor or assignment is unknown then
-      // we will not know the argument of the copied to object
-      if (!State->contains<T>(ArgMemRegion)) {// Think of the case when other is unknown
-        return State->remove<T>(ThisRegion);
-      }  
-      auto OtherQType = State->get<T>(ArgMemRegion);
-      auto OtherSVal = State->get<U>(ArgMemRegion);
-      State = State->set<U>(ThisRegion, *OtherSVal);
-      return State->set<T>(ThisRegion, *OtherQType);
-    } else if(isMoveConstructorCall(Call) || isMoveAssignemntCall(Call)) {
-      if (!State->contains<T>(ArgMemRegion)) {// Think of the case when other is unknown
+    bool IsCopy = isCopyConstructorCallEvent(Call) ||
+                                          isCopyAssignmentOperatorCall(Call);
+    bool IsMove = isMoveConstructorCall(Call) || isMoveAssignemntCall(Call);
+    if (IsCopy || IsMove) {
+      // If the argument of a copy constructor or assignment is unknown then
+      // we will not know the argument of the copied to object.
+      bool OtherQTypeKnown = State->contains<T>(ArgMemRegion);
+      bool OtherSValKnown = State->contains<U>(ArgMemRegion);
+
+      const QualType *OtherQType;
+      if (OtherQTypeKnown) {
+        OtherQType = State->get<T>(ArgMemRegion);
+      } else {
         return State->remove<T>(ThisRegion);
       }
+
+      const SVal *OtherSVal;
+      if (OtherSValKnown) {
+        OtherSVal =  State->get<U>(ArgMemRegion);
+      } else {
+        return State->remove<U>(ThisRegion);
+      }
+
+
+      // Get the information known abut the other object
+
+      // When move semantics is used we can only know that the moved from
+      // object must be in a destructible state. Other usage of the object
+      // than destruction is undefined.
+      if (IsMove) {
+        State = State->remove<T>(ArgMemRegion);
+        State = State->remove<U>(ArgMemRegion);
+      }
+      State = State->set<U>(ThisRegion, *OtherSVal);
+      return State->set<T>(ThisRegion, *OtherQType);
+    }
+    if (isCopyConstructorCallEvent(Call) ||
+                                          isCopyAssignmentOperatorCall(Call)) {
+      
+
+      // Get the QualType and SVal held by the other std::variant/std::any.
+      
+      // Set the QualType and SVal to the copied to object
+    } else if(isMoveConstructorCall(Call) || isMoveAssignemntCall(Call)) {
+      // Similarly to the copy constructor and assignment we have to check
+      // if we know information of the copied from std::variant/std::any.
+      if (!State->contains<T>(ArgMemRegion)) {
+        return State->remove<T>(ThisRegion);
+      }
+
       auto OtherQType = State->get<T>(ArgMemRegion);
-      State = State->remove<T>(ArgMemRegion);
+      auto OtherSVal = State->get<U>(ArgMemRegion);
+
+      State = State->set<U>(ThisRegion, *OtherSVal);
       return State->set<T>(ThisRegion, *OtherQType);
     } else {
       auto WoPointer = ArgTypePtr->getPointeeType();
