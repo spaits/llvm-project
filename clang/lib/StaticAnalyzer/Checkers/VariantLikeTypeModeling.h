047e5aa6a95e5b4b85f4883dc53e3b95f5371c70
Gabor Spaits <gaborspaits1@gmail.com>
2023-04-16 09:51:22 +0200

    add headers

--- clang/lib/StaticAnalyzer/Checkers/VariantLikeTypeModeling.h
+++ clang/lib/StaticAnalyzer/Checkers/VariantLikeTypeModeling.h
@@ -1,3 +1,11 @@
+//===- StdVariantChecker.cpp -------------------------------------*- C++ -*-==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef LLVM_CLANG_LIB_STATICANALYZER_CHECKER_VARIANTLIKETYPEMODELING_H
 #define LLVM_CLANG_LIB_STATICANALYZER_CHECKER_VARIANTLIKETYPEMODELING_H
 
