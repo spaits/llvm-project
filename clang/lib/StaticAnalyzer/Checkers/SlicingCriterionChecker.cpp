36b497828a8957a642f91fdf0f30f8cea31884c1
Gabor Spaits <gaborspaits1@gmail.com>
2025-03-25 15:16:02 +0100

    Flags to Specify Slicing Criterion

--- clang/lib/StaticAnalyzer/Checkers/SlicingCriterionChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/SlicingCriterionChecker.cpp
@@ -13,7 +13,13 @@
 #include "llvm/ADT/FoldingSet.h"
 #include "llvm/ADT/StringRef.h"
 #include <iterator>
-
+#include <string>
+// Run with: ninja clang && \
+// bin/clang++ --analyze -Xclang -analyzer-checker=core,alpha.core.SlicingCriterion \
+// -Xclang -analyzer-output=html \
+// -Xclang -analyzer-config -Xclang alpha.core.SlicingCriterion:LineNumber=12 \
+// -Xclang -analyzer-config -Xclang alpha.core.SlicingCriterion:ExpressionName=aaa \
+// test.c -o outp
 using namespace clang;
 using namespace ento;
 
@@ -41,6 +47,11 @@ public:
 };
 }; // end of namespace slicing
 
+struct SlicingCriterionOptions {
+  int LineNumber;
+  std::string ExpressionName;
+};
+
 static std::vector<const Expr *> findNamedExprsInStmt(const Stmt *S,
                                                       CheckerContext &C) {
   const SourceManager &SM = C.getSourceManager();
@@ -67,19 +78,28 @@ static std::vector<const Expr *> findNamedExprsInStmt(const Stmt *S,
 
 class SlicingCriterionChecker : public Checker<check::PreStmt<Stmt>> {
 public:
+  SlicingCriterionOptions Opts;
   void checkPreStmt(const Stmt *S, CheckerContext &C) const {
+    llvm::errs() << "SC: " << Opts.LineNumber << ":" << Opts.ExpressionName << '\n';
     llvm::errs() << "Entering Slicing Criterion checker for stmt: "
                  << S->getStmtClassName() << "\n";
     std::vector<const Expr *> ExpressionsInStmt = findNamedExprsInStmt(S, C);
   }
 };
 
+void clang::ento::registerSlicingCriterionChecker(
+    clang::ento::CheckerManager &Mgr) {
+  auto *Chk = Mgr.registerChecker<SlicingCriterionChecker>();
+
+  const AnalyzerOptions &AnOpts = Mgr.getAnalyzerOptions();
+  SlicingCriterionOptions &ChOpts = Chk->Opts;
+  ChOpts.LineNumber = AnOpts.getCheckerIntegerOption(Chk, "LineNumber");
+  ChOpts.ExpressionName = AnOpts.getCheckerStringOption(
+      Chk, "ExpressionName");
+}
+
+
 bool clang::ento::shouldRegisterSlicingCriterionChecker(
     clang::ento::CheckerManager const &mgr) {
   return true;
-}
-
-void clang::ento::registerSlicingCriterionChecker(
-    clang::ento::CheckerManager &mgr) {
-  mgr.registerChecker<SlicingCriterionChecker>();
 }
