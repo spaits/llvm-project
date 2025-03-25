fb0619a04f05bb3daf1af0b2f3b06031c81f0a29
Gabor Spaits <gaborspaits1@gmail.com>
2025-03-25 14:25:23 +0100

    Collect Expressions from a Statement

--- clang/lib/StaticAnalyzer/Checkers/SlicingCriterionChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/SlicingCriterionChecker.cpp
@@ -8,66 +8,78 @@
 #include "clang/StaticAnalyzer/Core/PathSensitive/CallDescription.h"
 #include "clang/StaticAnalyzer/Core/PathSensitive/CallEvent.h"
 #include "clang/StaticAnalyzer/Core/PathSensitive/CheckerContext.h"
+#include "clang/StaticAnalyzer/Core/PathSensitive/ProgramStateTrait.h"
 #include "clang/StaticAnalyzer/Core/PathSensitive/SVals.h"
 #include "llvm/ADT/FoldingSet.h"
 #include "llvm/ADT/StringRef.h"
-#include "clang/StaticAnalyzer/Checkers/BuiltinCheckerRegistration.h"
-#include "clang/StaticAnalyzer/Core/BugReporter/BugType.h"
-#include "clang/StaticAnalyzer/Core/Checker.h"
-#include "clang/StaticAnalyzer/Core/CheckerManager.h"
-#include "clang/StaticAnalyzer/Core/PathSensitive/CheckerContext.h"
-#include "clang/StaticAnalyzer/Core/PathSensitive/ProgramStateTrait.h"
 #include <iterator>
 
 using namespace clang;
 using namespace ento;
 
-void findExpressionInStmt(const Stmt *S, CheckerContext &C) {
-  const SourceManager &SourceManager = C.getSourceManager();
-  llvm::errs() << "Line: " << SourceManager.getSpellingLineNumber(S->getSourceRange().getBegin()) << '\n';
-  for (const auto *SubStmt : S->children()) {
-    //SubStmt->dump();
+namespace slicing {
+class NamedExprVisitor : public RecursiveASTVisitor<NamedExprVisitor> {
+public:
+  std::vector<const Expr *> NamedExprs;
+
+  bool VisitDeclRefExpr(DeclRefExpr *DRE) {
+    NamedExprs.push_back(DRE);
+    return true;
   }
 
-  class LocalVisitor : public RecursiveASTVisitor<LocalVisitor> {
-    public:
-      std::set<std::string> Vars;
-  
-      bool VisitDeclRefExpr(DeclRefExpr *DRE) {
-        if (const VarDecl *VD = dyn_cast<VarDecl>(DRE->getDecl())) {
-          Vars.insert(VD->getNameAsString());
-        }
-        return true;
-      }
-  
-      void traverse(const Stmt *S) {
-        TraverseStmt(const_cast<Stmt *>(S));
-      }
-    };
-  
-    LocalVisitor V;
-    V.traverse(S);
-  
-    for (const auto &VarName : V.Vars) {
-      llvm::errs() << "  Used variable: " << VarName << '\n';
+  bool VisitMemberExpr(MemberExpr *ME) {
+    NamedExprs.push_back(ME);
+    return true;
+  }
+
+  bool VisitUnresolvedLookupExpr(UnresolvedLookupExpr *ULE) {
+    NamedExprs.push_back(ULE);
+    return true;
+  }
+
+  void traverse(const Stmt *S) { TraverseStmt(const_cast<Stmt *>(S)); }
+};
+}; // end of namespace slicing
+
+static std::vector<const Expr *> findNamedExprsInStmt(const Stmt *S,
+                                                      CheckerContext &C) {
+  const SourceManager &SM = C.getSourceManager();
+  llvm::errs() << "Line: " << SM.getSpellingLineNumber(S->getBeginLoc())
+               << '\n';
+
+  slicing::NamedExprVisitor V{};
+  V.traverse(S);
+
+  for (const Expr *E : V.NamedExprs) {
+    E->dump(); // or print name with:
+    if (const auto *DRE = dyn_cast<DeclRefExpr>(E)) {
+      llvm::errs() << "  Name: " << DRE->getNameInfo().getAsString() << '\n';
+    } else if (const auto *ME = dyn_cast<MemberExpr>(E)) {
+      llvm::errs() << "  Member: " << ME->getMemberDecl()->getNameAsString()
+                   << '\n';
+    } else if (const auto *ULE = dyn_cast<UnresolvedLookupExpr>(E)) {
+      llvm::errs() << "  Unresolved name: " << ULE->getName().getAsString()
+                   << '\n';
     }
+  }
+  return V.NamedExprs;
 }
 
 class SlicingCriterionChecker : public Checker<check::PreStmt<Stmt>> {
 public:
   void checkPreStmt(const Stmt *S, CheckerContext &C) const {
-    llvm::errs() << "Entering Slicing Criterion checker for stmt: " << S->getStmtClassName() << "\n";
-    findExpressionInStmt(S, C);
+    llvm::errs() << "Entering Slicing Criterion checker for stmt: "
+                 << S->getStmtClassName() << "\n";
+    std::vector<const Expr *> ExpressionsInStmt = findNamedExprsInStmt(S, C);
   }
 };
-
 
 bool clang::ento::shouldRegisterSlicingCriterionChecker(
     clang::ento::CheckerManager const &mgr) {
   return true;
 }
 
-void clang::ento::registerSlicingCriterionChecker(clang::ento::CheckerManager &mgr) {
-    mgr.registerChecker<SlicingCriterionChecker>();
-  }
-  
+void clang::ento::registerSlicingCriterionChecker(
+    clang::ento::CheckerManager &mgr) {
+  mgr.registerChecker<SlicingCriterionChecker>();
+}
